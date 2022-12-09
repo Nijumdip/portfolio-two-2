f390b212940889d01f4f00c2a83e76579428c5cf
Nijumdip <nijumdip019911@gmail.com>
2022-12-09 12:37:02 +0600

    done about page

--- Components/aboutCard.js
+++ Components/aboutCard.js
@@ -3,14 +3,12 @@ import React from "react";
 const AboutCard = ({ card }) => {
     const { img, name, description, _id } = card;
     return (
-        <div className=" bg-base-100 h-80 bg-pink-100 p-6 rounded-3xl shadow-md flex flex-row gap-2">
+        <div className=" bg-base-100 h-80 bg-pink-100 p-6 rounded-3xl shadow-md flex flex-row gap-1">
             <figure >
                 <img src={img} alt="site name" className="rounded-full p-2 " />
             </figure>
             <div>
-                <h5  class=" text-xl font-bold">{name}</h5>
-                <h5  class=" text-xl font-bold">{name}</h5>
-                <h5  class=" text-xl font-bold">{name}</h5>
+                <h5  class=" text-lg font-bold">{name}</h5>
                 <p class="font-normal text-gray-700">{description}</p>
             </div>
         </div>
