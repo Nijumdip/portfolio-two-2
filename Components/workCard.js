7066ea1a19650d6f083c4c706df1c97d8d72a057
Nijumdip <nijumdip019911@gmail.com>
2022-12-10 14:57:08 +0600

    some changes work card

--- Components/workCard.js
+++ Components/workCard.js
@@ -5,9 +5,9 @@ const WorkCard = ({ card}) => {
     return (
         <>
             <div className=" bg-base-100 h-80 bg-pink-100 p-6 rounded-3xl shadow-md flex flex-col gap-1">
-                <figure>
-                    <img src={img} alt="site name" className="rounded-full p-2 " />
-                </figure>
+                <div>
+                    <img src={img} alt="site name" className="rounded-3xl w-full p-2 " />
+                </div>
                 <div>
                     <h5 class=" text-lg font-bold">{title}</h5>
                     <p class="font-normal text-gray-700">{name}</p>
